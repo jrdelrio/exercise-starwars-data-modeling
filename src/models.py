e31c3daac3cfaed6c2c3e4787e7a5d7ddbc91ef2
Jose Raimundo Del Rio <jrdelriodom@gmail.com>
2023-01-16 18:26:51 +0000

    diagram.png completed

--- src/models.py
+++ src/models.py
@@ -8,26 +8,49 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Character(Base):
+    __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    name = Column(String(250), nullable=True)
+    height = Column(Integer)
+    mass = Column(Integer)
+    gender = Column(String)
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'))
+    url = Column(String, unique=True)
+    
+class Planet(Base):
+    __tablename__ = 'planet'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    diameter = Column(Integer)
+    rotation_period = Column(Integer)
+    gravity = Column(Integer, nullable=False)
+    population = Column(Integer)
+    surface_water = Column(Integer)
+    url = Column(String)
 
     def to_dict(self):
         return {}
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    id = Column(Integer, primary_key=True)
+    charac_id = Column(Integer, ForeignKey('character.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+ 
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    id = Column(Integer, primary_key=True)
+    model = Column(String)
+    starship_class = Column(String)
+    manufacturer = Column(String)
+    url = Column(Integer)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
